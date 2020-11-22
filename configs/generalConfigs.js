45686534d987121290be9ba480bcaa5bb073f88d
Ygor Carlos Ferreira Rolim <ygor.rolim@radixeng.com.br>
2020-11-22 19:53:29 -0300

    Updating generalConfigs

--- configs/generalConfigs.js
+++ configs/generalConfigs.js
@@ -1,7 +1,7 @@
 module.exports = {
     aws : {
-        accessKey: 'AKIAIBGZ4EL2JR76YWNA',
-        secretKey: '0Gy18HgdW+G4GHyKXJQc9aDjznChrMGT8hfmv4kA',
-        bucketName: 'solvimm-ycfr-bucket-challenge'
+        accessKey: 'YOUR ACCESS KEY',
+        secretKey: 'YOUR SECRET KEY',
+        bucketName: 'YOUR BUCKET NAME'
     }
-}
+} 
